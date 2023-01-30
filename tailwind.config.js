2f74b0d00a6e758c93558764ba13e81c75552f53
Mostafizur Rahman Nayem <hpbd17062022@gmail.com>
2023-01-30 07:41:48 +0600

    Work out at home for free section done alhmdulilha

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./dist/*{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#171717',
+        blueblack: ' #232532',
+        orrange: '#E6533C',
+        unknowColur : '#7A29DC',
+        darkWhite : '#FAFAF9',
+        lightBlack: '#303030',
+        lightBlack2  : '#323443',
+        gray : '#4F5261',
+      },
+    },
+    fontFamily: {
+      inter : ['Inter', 'sans-serif'],
+      poppins : ['Poppins', 'sans-serif'],
+    }
   },
   plugins: [require("daisyui")],  // daisyUI config (optional)
   daisyui: {
@@ -13,7 +28,7 @@ module.exports = {
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "false",
+    darkTheme: "black",
   },
 
 }
